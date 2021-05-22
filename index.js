3245c6fcaa00f8a01259cb18312ddd0f7883119c
steven <undefined>
2021-05-22 23:49:35 +0100

    Add headers/scripts for loading QuickSight

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-'use strict';
+"use strict";
 
-var fs = require('fs');
-var path = require('path');
+var fs = require("fs");
+var path = require("path");
 
-exports.get = function(event, context, callback) {
+exports.get = function (event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
   var result = {
     statusCode: 200,
     body: contents.toString(),
-    headers: {'content-type': 'text/html'}
+    headers: {
+      "content-type": "text/html",
+      "Content-Security-Policy":
+        "default-src 'unsafe-inline' https://us-east-1.quicksight.aws.amazon.com https://s3-eu-west-1.amazonaws.com https://unpkg.com; frame-ancestors https://*.quicksight.aws.amazon.com",
+    },
   };
 
   callback(null, result);

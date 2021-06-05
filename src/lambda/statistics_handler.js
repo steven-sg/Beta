27937e7d661ca0f7ed2210c2d18e09773cada669
steven <undefined>
2021-06-05 23:49:47 +0100

    Configure minimal html file for statistics endpoint

--- src/lambda/statistics_handler.js
+++ src/lambda/statistics_handler.js
@@ -1,10 +1,18 @@
 "use strict";
 
+var fs = require("fs");
+var path = require("path");
+
 exports.get = function (event, context, callback) {
+  var contents = fs.readFileSync(`public${path.sep}statistics.html`);
   var result = {
     statusCode: 200,
-    body: "success",
-    headers: { "content-type": "text/html" },
+    body: contents.toString(),
+    headers: {
+      "content-type": "text/html",
+      "Content-Security-Policy":
+        "default-src 'unsafe-inline' https://eu-west-1.quicksight.aws.amazon.com https://s3-eu-west-1.amazonaws.com https://unpkg.com; frame-ancestors https://*.quicksight.aws.amazon.com",
+    },
   };
 
   callback(null, result);
